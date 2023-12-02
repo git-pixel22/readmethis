9625711d9c46b74e2c06f1fd7073031050d0180d
Rajat Sharma <offciallycurious@gmail.com>
2023-12-02 11:41:52 +0530

    done

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 function speak(){
         
     let inputText = document.getElementById("inputText");
+    var audio = new Audio('btn-tap.mp3');
 
     let utterance = new SpeechSynthesisUtterance(inputText.value);
+
+    audio.play()
     speechSynthesis.speak(utterance);
-    }
+}
